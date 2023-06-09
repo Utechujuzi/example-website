f3e770dfca6303ac76870391c03afcf2945dffe5
utechujuzi <utech.ujuzi@gmail.com>
2023-06-10 02:11:37 +0300

    another day

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/upload')
-def upload():
-    redirect('upload_assignment.html')
+@app.route('/signup')
+def signup():
+    return render_template('registration.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html') 
 
     app.run(debug=True)
